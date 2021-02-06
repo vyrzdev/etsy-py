383107055f2574a5616a065d84d7013d978b7e60
Ben Parsons-Willis <ben@vyrz.dev>
2021-02-06 13:52:55 +0000

    Initial Boilerplate

---
+++ src/client.py
@@ -0,0 +1,13 @@
+import requests
+
+class EtsyResponse:
+    pass
+
+
+class EtsyClient:
+    def __init__(self, api_base_url="https://openapi.etsy.com/v2"):
+        self.api_base_url = api_base_url
+
+    def get_etsy_methods(self):
+        pass
+
