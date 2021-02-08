97e63f1186a7db710971fb8b17f87cabf7b0a4bd
Ben Parsons-Willis <ben@vyrz.dev>
2021-02-08 01:57:35 +0000

    PyPi

---
+++ script.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme = fh.read()
+
+setuptools.setup(
+    name="etsy-py-dev",
+    version="0.0.1",
+    author="vyrzdev",
+    author_email="ben@vyrz.dev",
+    description="An auto-updating Etsy Wrapper",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vyrzdev/etsy-py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
