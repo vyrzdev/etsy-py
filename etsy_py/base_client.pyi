12b83233f4f49b550e8b76fcadd0479c543bad7a
Ben Parsons-Willis <ben@vyrz.dev>
2021-02-11 16:33:11 +0000

    Overhauled autogeneration by using Jinja2

--- etsy_py/base_client.pyi
+++
@@ -1,5 +0,0 @@
-from . import rq
-import typing
-
-class EtsyClient:
-    def __init__(self, requester: rq.EtsyRequester, api_base_url: str = ..., debug: bool = ...) -> EtsyClient:  ...
--- method_generator.py
+++ method_generator.py
@@ -1,154 +1,47 @@
-import re
+import jinja2
+import dataclasses
+import typing
 import json
-
-with open("current_method_table.json", "r") as method_table_file:
-    method_table_json_str = method_table_file.read()
-    methods_table = json.loads(method_table_json_str)
-
-print(f"Found {methods_table.get('count')} methods in current_method_table.json")
-
-methods_file = open("etsy_py/methods.py", "r")
-
-currently_implemented_methods = dict()
-
-capture = False
-for line in methods_file.readlines():
-    if "# StartMethod:" in line:
-        capture = True
-        method_name = re.search(": ?(\w+)", line).group(1)
-        method_code = ""
-    elif "# EndMethod:" in line:
-        capture = False
-        ended_method = re.search(": ?(\w+)", line).group(1)
-        if ended_method != method_name:
-            print(f"UNFINISHED METHOD: {method_name}")
-            exit()
-        else:
-            currently_implemented_methods.update(**{method_name: method_code})
-            ended_method = None
-            method_name = None
-    else:
-        if capture:
-            method_code += line
-        else:
-            pass
-
-if capture:
-    print(f"UNFINISHED METHOD: {method_name}")
-    exit()
+import re
 
 
-def generate_method_code(method_definition_json: dict) -> list:
-    params: dict = method_definition_json.get("params")
-    if params is None:
-        params = {}
-
-    uri = method_definition_json["uri"]
-    uri_params = re.findall(':(\w+)(?:\/|$)', uri)
-    definition_line = f"def {method_definition_json['name']}(self,"
-    args = ""
-    for param in uri_params:
-        try:
-            args += f" {param},"
-        except KeyError:
-            print(f"Incorrectly written method_definition for method: {method_definition_json['name']}\n"
-                  f"Missing a required parameter: {param}\n"
-                  f"Ignoring for now...",
-                  )
-            args += f" {param},"
-
-    if method_definition_json["http_method"] in ["POST", "PUT"]:
-        args += " data=None,"
-
-    args += " query_params=None"
-    definition_line += args
-    definition_line += "):"
-
-    if method_definition_json["visibility"] == "private":
-        oauth_check_lines = [
-            f"    if self.requester.auth_mode == 'api_key':",
-            f"        raise RequiresOauth"
-        ]
-    else:
-        oauth_check_lines = []
-
-    uri_param_string = "["
-    for uri_param in uri_params:
-        uri_param_string += f" {uri_param},"
-    uri_param_string += "]"
-    request_line = f"    return self.requester.make_request(uri='{method_definition_json['uri']}', uri_params={uri_param_string}, method='{method_definition_json['http_method']}', query_params=query_params,"
-    if method_definition_json["http_method"] in ["POST", "PUT"]:
-        request_line += " data=data,"
-
-    request_line += ")"
-
-    code = [
-        definition_line,
-        *oauth_check_lines,
-        request_line
-    ]
-    return code
+env = jinja2.Environment(
+    loader=jinja2.PackageLoader('method_generator', 'auto_generation/templates'),
+)
 
 
-method_definitions = {}
-for method in methods_table.get("results"):
-    code = "\n".join(generate_method_code(method))
-    method_definitions.update(**{method["name"]: code})
-
-with open("etsy_py/methods.py", "w") as build_file:
-    file_header_lines = [
-        "from .rq import RequiresOauth\n",
-        "\n# These methods are all auto-generated from the Etsy Method Table, by method_generator.py periodically",
-        "# Any manual changes will be overwritten!\n"
-    ]
-    header = "\n".join(file_header_lines)
-
-    methods_raw = ""
-    for method, code in method_definitions.items():
-        method_raw = f"\n# StartMethod: {method}\n{code}\n# EndMethod: {method}\n\n"
-        methods_raw += method_raw
-
-    build = f"{header}\n{methods_raw}"
-
-    build_file.write(build)
+@dataclasses.dataclass
+class Method:
+    name: str
+    uri: str
+    oauth_required: bool
+    uri_parameters: typing.List[str]
+    http_method: str
 
 
-def generate_stub_definition(method_definition_json):
-    params: dict = method_definition_json["params"]
-    if params is None:
-        params = {}
+with open("current_method_table.json", "r") as current_method_table_file:
+    method_table = json.loads(current_method_table_file.read())
 
-    uri = method_definition_json["uri"]
-    uri_params = re.findall(':(\w+)(?:\/|$)', uri)
-    definition_line = f"    def {method_definition_json['name']}(self,"
-    args = ""
-    for param in uri_params:
-        try:
-            args += f" {param},"
-        except KeyError:
-            print(f"Incorrectly written method_definition for method: {method_definition_json['name']}\n"
-                  f"Missing a required parameter: {param}\n"
-                  f"Ignoring for now...",
-                  )
-            args += f" {param},"
+built_methods: typing.List[Method] = list()
+for method_json in method_table.get("results"):
+    uri_parameters = re.findall(':(\w+)(?:\/|$)', method_json.get("uri"))
+    print(uri_parameters)
 
-    if method_definition_json["http_method"] in ["POST", "PUT"]:
-        args += " data: typing.Union[None, dict] = ...,"
+    built_methods.append(Method(
+        name=method_json.get("name"),
+        uri=method_json.get("uri"),
+        oauth_required=(method_json.get("visibility") == "private"),
+        uri_parameters=uri_parameters,
+        http_method=method_json.get("http_method")
+    ))
 
-    args += " query_params: typing.Union[None, dict] = ..."
-    definition_line += args
-    definition_line += ") -> rq.EtsyResponse: ..."
+new_methods_py_str = env.get_template("methods_py_template.jinja2").render(methods=built_methods)
+with open("etsy_py/methods.py", "w") as new_methods_py_file:
+    new_methods_py_file.write(new_methods_py_str)
 
-    return "\n".join([
-        definition_line
-    ])
+new_client_pyi_str = env.get_template("client_pyi_template.jinja2").render(methods=built_methods)
+with open("etsy_py/client.pyi", "w") as new_client_pyi_file:
+    new_client_pyi_file.write(new_client_pyi_str)
 
 
-stub_file_code = ""
-for method in methods_table["results"]:
-    stub_file_code += f"\n{generate_stub_definition(method)}\n"
 
-with open("etsy_py/base_client.pyi", "r") as base_client_stub:
-    with open("etsy_py/client.pyi", "w") as build_file:
-        raw = f"{base_client_stub.read()}\n    # Autogenerated Stubs...\n{stub_file_code}"
-        build_file.write(raw)
